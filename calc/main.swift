4110151a0532c279b4b6d60bd1e56c6346dfba8d
Quang Huy Vu <vuquanghuy1998@gmail.com>
2026-03-21 17:00:58 +1100

    Initial Commit

---
+++ calc/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  calc
+//
+//  Created by Jesse Clark on 12/3/18.
+//  Copyright © 2018 UTS. All rights reserved.
+//
+
+import Foundation
+
+var args = ProcessInfo.processInfo.arguments
+args.removeFirst() // remove the name of the program
+
+// Initialize a Calculator object
+let calculator = Calculator()
+do {
+    let result = try calculator.calculate(args: args)
+    print(result)
+} catch {
+    fputs("\(error)\n", stderr)
+    exit(1)
+}
